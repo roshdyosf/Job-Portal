8ec88c31ed1f1e7d038b4b13d0abf2e1da2886a4
roshdyosf <yosef2236303@gmail.com>
2026-07-28 06:30:44 +0300

    update Job and Tag models to specify foreign and related pivot keys for many-to-many relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -17,7 +17,7 @@ class Job extends Model
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
     }
 
 }
