3bdf7e08734ed020551be4e33154282bb46a223a
roshdyosf <yosef2236303@gmail.com>
2026-07-30 09:44:07 +0300

    add fruitcake/laravel-debugbar to development dependencies; update composer.lock and jobs view for improved job listing display

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,13 +1,16 @@
 <x-layout>
     <x-slot:heading>Jobs</x-slot:heading>
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
             <br>
             <li>
-                <a class=" text-gray-500 hover:underline" href="/jobs/{{$job['id']}}">
-                    <strong>{{$job['title']}}</strong> : pays {{$job['salary']}}
+                <a class="block px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-100" href="/jobs/{{$job['id']}}">
+                    <div class="font-bold text-blue-500">{{$job->employer->name}}</div>
+                    <div>
+                        <strong>{{$job['title']}}</strong> : pays {{$job['salary ']}} per year
+                    </div>
                 </a>
             </li>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
