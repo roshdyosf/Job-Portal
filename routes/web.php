1f7163b55536669acfc7392f48463ed02a93a294
roshdyosf <yosef2236303@gmail.com>
2026-08-01 05:06:42 +0300

    refactor job views and routes: restructure job listing and detail views, add create job view

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 });
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
@@ -20,9 +20,13 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 
 });
 
