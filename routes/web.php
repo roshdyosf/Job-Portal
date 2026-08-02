00d1bdde3a127231481bbb0c6ebea4ad21114aa4
roshdyosf <yosef2236303@gmail.com>
2026-08-02 07:50:07 +0300

    ps

--- routes/web.php
+++ routes/web.php
@@ -16,28 +16,25 @@ Route::get('/', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
-//______________________________________________________________
-
-//job routes
-
+/*
+--------------------------------------------------------------
+job routes
+--------------------------------------------------------------
+*/
 //index
 Route::get('/jobs', [JobController::class, 'index']);
-
 
 //create
 Route::get('/jobs/create', [JobController::class, 'create']);
 
-
 //store
 Route::Post('/jobs', [JobController::class, 'store']);
-
 
 //edit
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
 
 //update
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
 
 //destroy
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
