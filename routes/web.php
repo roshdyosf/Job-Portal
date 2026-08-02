7559f334110397545ced885c55befaa52859bdc8
roshdyosf <yosef2236303@gmail.com>
2026-08-02 12:59:47 +0300

    feat: update authentication routes to include login functionality

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisterUserController;
-
+use App\Http\Controllers\SessionController;
 /*
 --------------------------------------------------------------
 Home route
@@ -27,7 +27,9 @@ Auth routes
 --------------------------------------------------------------
 */
 Route::get('/register', [RegisterUserController::class, 'create'])->name('register');
-Route::view('/login', 'users.login');
+Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
+Route::get('/login', [SessionController::class, 'create'])->name('login');
+Route::post('/login', [SessionController::class, 'store'])->name('login.store');
 
 
 
